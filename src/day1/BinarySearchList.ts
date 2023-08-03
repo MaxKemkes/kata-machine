0915bb4404dc55c97620063135c16f13f3ef8264
Maximilian Kemkes <maximilian.kemkes@gmail.com>
2023-08-03 08:33:59 +0200

    .

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,19 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-  let lo = 0
-  let hi = haystack.length
-  
-  while(lo < hi){
-    const m = Math.floor(lo + (hi-lo)/2)
-    const e = haystack[m]
+    let lo = 0
+    let hi = haystack.length
 
-    if (e === needle){
-      return true
-    } else if (e < needle){
-      lo = m +1
-    } else {
-      hi = m
+    while(lo < hi){
+        const mid = Math.floor(lo + (hi-lo)/2)
+        const el = haystack[mid]
+
+        if (el === needle){
+        return true
+        } else if (el < needle){
+        lo = mid +1
+        } else {
+        hi = mid
+        }
     }
-  }
 
-  return false
+    return false
 }
