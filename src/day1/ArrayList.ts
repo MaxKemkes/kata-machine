adfc1dadcee33d8557a875bccd5749a98ac64eb2
Maximilian Kemkes <109769614+MaxKemkes@users.noreply.github.com>
2023-08-08 13:05:24 +0200

    .

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,95 @@
 export default class ArrayList<T> {
     public length: number;
-
+    public capacity: number;
+    public arr:Array<T | undefined>;
     
 
-    constructor() {
+    constructor(x:number) {
+        this.length = 0
+        this.capacity = x
+        this.arr = new Array<T>(x)
     }
 
     prepend(item: T): void {
+        this.length++;
+        const temp = this.arr
+        if (this.length === this.capacity){
+            //create new Array
+            this.arr = new Array(this.capacity * 2)
+        }
 
-}
+        for (let i = 0; i <= this.length; i++){
+            this.arr[i + 1] = temp[i]
+        }
+
+        this.arr[0] = item
+    }
+
     insertAt(item: T, idx: number): void {
+        this.length++;
+        const temp = this.arr
+        if(this.length === this.capacity){
+            this.arr = new Array(this.capacity * 2)
+        }
 
-}
+        this.arr[idx] = item
+
+        if (idx > 0){
+            for (let i = 0; i < idx; i++){
+                this.arr[i] = temp[i]
+            }
+        }
+
+        for (let i = idx; i < this.length - 1; i++){
+            this.arr[i + 1] = temp[i]
+        }
+    }
+
     append(item: T): void {
+        if(this.length === this.capacity){
+            //create new Array
+            const arr = new Array(this.capacity * 2)
+            for (let i = 0; i < this.length; i++){
+                arr[i] = this.arr[i]
+            }
+            
+            this.arr = arr
+        }
+        this.arr[this.length] = item
+        this.length++;
+    }
 
-}
     remove(item: T): T | undefined {
+        for (let i = 0; i < this.length; i++ ){
+            if ( this.arr[i] === item){
+                for ( let j = i ; i < this.length - 1; i++){
+                    this.arr[j] = this.arr[j+1]
+                }
 
-}
+                this.length --
+                this.arr[this.length] = undefined
+
+                return item
+            }
+        }
+        return undefined
+    }
+
     get(idx: number): T | undefined {
+        return idx < this.length ? this.arr[idx] : undefined
+    }
 
-}
     removeAt(idx: number): T | undefined {
+        if (idx >= this.length){
+            return undefined
+        }
 
-}
+        const val = this.arr[idx]
+        this.length--;
+        for (let i = idx; i < this.length; i++){
+            this.arr[i] = this.arr[i+1]
+        }
+        this.arr[this.length] = undefined
+        return val
+    }
 }
--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,25 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
 
+    // base cases
+    // no more node
+    if(!curr){
+        return false
+    }
+    // node with searched value
+    if(curr.value === needle){
+        return true
+    }
+
+    //recurse
+    // if value smaller than search value ==> go right
+    if(curr.value < needle){
+        return search(curr.right, needle)
+    }
+    // if value bigger than search value ==> go left
+    return search(curr.left, needle)
+
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle)
 }
